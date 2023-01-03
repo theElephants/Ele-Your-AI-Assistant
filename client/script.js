1c079dc68cf597ca83001081ecdd75257a9ef561
Kershner <ekershner@eiccd.net>
2023-01-03 12:51:04 -0600

    update the server url

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async(e) => {
     loader(messageDiv);
 
     //fetch data from server -> bot's response
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://ele-ai-assistant.onrender.com', {
         method: 'POST',
         headers: {
             'Authorization': 'Bearer',
